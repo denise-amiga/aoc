2180009407dbcb44cd7a424aec005d6bd0860a13
Denise <denise_amiga@hotmail.com>
2022-04-22 17:51:39 +0200

    2015 problem 01-02

--- aoc2015/day01/s2.py
+++ aoc2015/day01/s2.py
@@ -6,5 +6,14 @@ from basic import *
 t0 = time()
 i_txt = my_read_input()
 
+lvl = 0
+for x, c in enumerate(i_txt):
+    if c == '(':
+        lvl += 1
+    elif c == ')':
+        lvl -= 1
+    if lvl == -1:
+        break
+print(x + 1)  # base 0
 t1 = time()
 print('time: ', t1 - t0)
