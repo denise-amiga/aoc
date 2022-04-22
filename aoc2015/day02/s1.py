a3e8bc467d4eded43a40918ccb8b5a3b72203adb
Denise <denise_amiga@hotmail.com>
2022-04-22 18:28:47 +0200

    2015 problem 02-01

--- aoc2015/day02/s1.py
+++ aoc2015/day02/s1.py
@@ -4,7 +4,9 @@ sys.path.insert(0, '../../')  # noqa: E402
 from basic import *
 
 t0 = time()
-i_txt = my_read_input()
+i_raw = my_read_input()
+i_txt = [list(map(int, x.split('x'))) for x in i_raw.splitlines()]
 
+print(sum(2 * (a * b + b * c + a * c) + min(a * b, b * c, a * c) for a, b, c in i_txt))
 t1 = time()
 print('time: ', t1 - t0)
